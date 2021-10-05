6199aeb48a5dc0fc9f7307f4b61d647485ad1fe3
Stephan Van Hemelrijck <Stephan.van.hemelrijck@student.ehb.be>
2021-10-05 16:43:15 +0200

    Pokemons API - Load it onto html

---
+++ Les2/js/pokemon.js
@@ -0,0 +1,43 @@
+"use strict;";
+
+const pokemon = {
+  pokemonFetch() {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+      .then((response) => response.json())
+      .then((data) => {
+        let newHTMLString = "";
+        const pokemonNames = [];
+        const pokemonURLs = [];
+        const pokemons = data.results;
+        pokemons.forEach((pokemon) => {
+          fetch(pokemon.url)
+            .then((res) => res.json())
+            .then((data) => {
+              let htmlString = `
+              <div id="container-box-pk">
+        <figure>
+          <img
+            src="${data.sprites.front_default}"
+            alt="bulbasaur"
+            class="pokemon-img"
+          />
+          <figcaption>Nr.999</figcaption>
+        </figure>
+        <h1 class="pokemon-name">${data.forms[0].name}</h1>
+        <ul class="pokemon-traits">
+          <li><p>Grass</p></li>
+          <li><p>Poison</p></li>
+        </ul>
+        <button class="add-to-team">Add to team</button>
+      </div>
+              `;
+              newHTMLString += htmlString;
+              document.getElementById("container-box").innerHTML =
+                newHTMLString;
+            });
+        });
+      });
+  },
+};
+
+pokemon.pokemonFetch();
